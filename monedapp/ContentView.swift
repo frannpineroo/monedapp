d40dd5af504e0e839a46d17a2a4e4b1d913fb63e
Francisco Piñero <135169832+frannpineroo@users.noreply.github.com>
2026-07-18 19:29:03 -0300

    Initial Commit

---
+++ monedapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  monedapp
+//
+//  Created by Francisco Pinero on 18/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ monedapp/monedappApp.swift
@@ -0,0 +1,17 @@
+//
+//  monedappApp.swift
+//  monedapp
+//
+//  Created by Francisco Pinero on 18/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct monedappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
